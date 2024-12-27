0275d78c65195ae120f69188eb34a1ed2f164e7d
rumesh <rumeshk066@gmail.com>
2024-12-27 12:39:49 +0530

    Add bg-gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Todo from "./components/Todo"
 
 function App() {
 
   return (
     <>
-    
+    <div className="grid min-h-screen py-4 bg-gradient-to-bl from-[#0f172a] via-[#1e1a78] to-[#0f172a]">
+
+
+    <Todo/>
+    </div>
     </>
   )
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+const Todo = () => {
+  return (
+    <>
+    <div className="bg-white place-self-center w-11/12 flex flex-col max-w-md p-7 min-h-[550px] rounded-xl">
+      dnvbbk
+
+    </div>
+    </>
+  )
+}
+
+export default Todo
