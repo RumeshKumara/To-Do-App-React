a079529fbae4ab38c230e5c1d4f5b5d4557274fa
rumesh <rumeshk066@gmail.com>
2024-12-31 17:12:33 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ function App() {
   return (
     <>
     <div className="grid min-h-screen py-4 bg-gradient-to-bl from-[#0f172a] via-[#1e1a78] to-[#0f172a]">
-
-
-    <Todo/>
-    <div className="fixed bottom-0 right-0 m-4 md:m-6 lg:m-8">
-      <h3 className="text-slate-300"><span className="text-lg font-bold text-purple-500">#</span>Rumesh</h3>
-    </div>
+      <Todo/>
+      <div className="fixed bottom-0 right-0 m-4 md:m-6 lg:m-8">
+        <h3 className="text-slate-300"><span className="text-lg font-bold text-purple-500">#</span>Rumesh</h3>
+      </div>
     </div>
     </>
   )
